b84d2f1e20d7b234a2e070486587a2acaecbf149
Emilie <emilie.k.quartey@gmail.com>
2021-02-23 13:57:29 +0000

    initial commit

--- COVID_19/COVID_19/settings.py
+++ COVID_19/COVID_19/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c4x@6#dr04sl3ixf@$xb@66_xvcyc%7i#w_t=p0#x!-k+08a3j'
+SECRET_KEY = 'h88dxgfe-4-v-&y3=-nvcz7v%bkvlxb7ugf52==+eh4x4*&uy_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Speech.apps.SpeechConfig',
 ]
 
 MIDDLEWARE = [
--- COVID_19/Speeches/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpeechesConfig(AppConfig):
-    name = 'Speeches'
